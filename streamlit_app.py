62d3cc9104dcb5c1cc300a01a21f1351c721f5f6
Amandine Thivet <amandine.bresil@free.fr>
2022-11-23 10:23:57 +0100

    Update streamlit_app.py
    Added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
-streamlit.title('Hello there')
+streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('What\'s up ?')
-streamlit.text('Very good and u')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
